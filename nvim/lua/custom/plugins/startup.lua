1901875f6400e52def0e1d9a3d7314ceaa4b4d31
travesties <travishuntt@proton.me>
2024-04-07 09:58:24 -0400

    Add Neovim Startup Screen

---
+++ nvim/lua/custom/plugins/startup.lua
@@ -0,0 +1,82 @@
+return {
+  'startup-nvim/startup.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-neo-tree/neo-tree.nvim',
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('startup').setup {
+      header = {
+        type = 'text',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Header',
+        margin = 5,
+        content = {
+          ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+          ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+          ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+          ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+          ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+          ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+        },
+        highlight = 'Statement',
+        default_color = '',
+        oldfiles_amount = 0,
+      },
+      body = {
+        type = 'mapping',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Basic Commands',
+        margin = 5,
+        content = {
+          { 'Find File', 'Telescope find_files', '<leader>sf' },
+          { 'Find Word', 'Telescope live_grep', '<leader>sw' },
+          { 'Recent Files', 'Telescope oldfiles', '<leader>s.' },
+          { 'File Browser', ':Neotree position=float<CR>', '<leader>pv' },
+          { 'Colorschemes', 'Telescope colorscheme', '<leader>cs' },
+          { 'New File', "lua require'startup'.new_file()", '<leader>nf' },
+        },
+        highlight = 'String',
+        default_color = '',
+        oldfiles_amount = 0,
+      },
+      footer = {
+        type = 'text',
+        oldfiles_directory = false,
+        align = 'center',
+        fold_section = false,
+        title = 'Footer',
+        margin = 5,
+        content = { 'startup.nvim' },
+        highlight = 'Number',
+        default_color = '',
+        oldfiles_amount = 0,
+      },
+
+      options = {
+        mapping_keys = true,
+        cursor_column = 0.5,
+        empty_lines_between_mappings = true,
+        disable_statuslines = true,
+        paddings = { 1, 3, 3, 0 },
+      },
+      mappings = {
+        execute_command = '<CR>',
+        open_file = 'o',
+        open_file_split = '<c-o>',
+        open_section = '<TAB>',
+        open_help = '?',
+      },
+      colors = {
+        background = '#1f2227',
+        folded_section = '#56b6c2',
+      },
+      parts = { 'header', 'body', 'footer' },
+    }
+  end,
+}
