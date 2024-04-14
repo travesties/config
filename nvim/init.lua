d8c9933d07936998a637b16f6b8c8cf362501c72
travesties <travishuntt@proton.me>
2024-04-14 19:11:51 -0400

    Add leap navigation plugin
    Leap and mini.surround had a key conflict, so this also customizes the
    mini.surround keybindings.

--- nvim/init.lua
+++ nvim/init.lua
@@ -825,7 +825,22 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        custom_surroundings = nil,
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          add = 'ma', -- Add surrounding in Normal and Visual modes
+          delete = 'md', -- Delete surrounding
+          find = 'mf', -- Find surrounding (to the right)
+          find_left = 'mF', -- Find surrounding (to the left)
+          highlight = 'mh', -- Highlight surrounding
+          replace = 'mr', -- Replace surrounding
+          update_n_lines = 'mn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
