478c7a359a536c3922e11ea73da9f13279ae698b
travesties <travishuntt@proton.me>
2024-06-12 08:20:44 -0400

    Audio controls

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ if [ ! -d "$GOINSTALL" ]; then
 fi
 
 ### i3 window manager
-sudo apt -y install i3 nitrogen picom rofi
+sudo apt -y install i3 nitrogen picom rofi pulseaudio
 ln -sTf "$PWD/i3" "$XDG_CONFIG_HOME/i3"
 ln -sTf "$PWD/picom" "$XDG_CONFIG_HOME/picom"
 ln -sTf "$PWD/nitrogen" "$XDG_CONFIG_HOME/nitrogen"
