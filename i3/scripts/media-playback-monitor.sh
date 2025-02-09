19da6687299c94dd1f186b879e24439566184492
travesties <travishuntt@proton.me>
2025-02-09 17:50:28 -0500

    Add i3 script to prevent screensaver while playing media

---
+++ i3/scripts/media-playback-monitor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Use first argument as the interval timer. Default to 55 seconds.
+while sleep "${1:-55}"
+do
+    # Build list of dbus services that implement the org.mpris.MediaPlayer2 interface
+    media_players=( $(dbus-send \
+        --print-reply \
+        --dest=org.freedesktop.DBus  \
+        /org/freedesktop/DBus \
+        org.freedesktop.DBus.ListNames \
+        | egrep -o '"org.mpris.MediaPlayer2.*"' \
+        | sed 's/"//g'))
+
+    # Search media players for a playback status of "Playing"
+    for player in $media_players; do
+        status=$(\
+            dbus-send \
+                --print-reply=literal \
+                --dest=$player \
+                /org/mpris/MediaPlayer2 \
+                org.freedesktop.DBus.Properties.Get \
+                string:org.mpris.MediaPlayer2.Player \
+                string:PlaybackStatus \
+        )
+
+        # Disable screen saver if media is playing
+        if [[ "$status" == *"Playing"* ]]; then
+            xset s reset
+            break
+        fi
+    done
+done
