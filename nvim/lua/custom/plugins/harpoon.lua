1216d891bd53ca5adf6db28bf8a57a0c775cea5e
travesties <travishuntt@proton.me>
2025-02-20 10:06:44 -0500

    Remove numbered file harpoon keybindings

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -28,15 +28,6 @@ return {
       },
     }
 
-    for i = 1, 5 do
-      table.insert(keys, {
-        '<leader>' .. i,
-        function()
-          require('harpoon'):list():select(i)
-        end,
-        desc = 'Harpoon to File ' .. i,
-      })
-    end
     return keys
   end,
 }
